7b6102f541bb6a00220a1de346f891647ed2a134
Superman37891 <sarthakbansal000@gmail.com>
2026-04-29 12:39:29 -0500

    Fixed preview download and description for yearly / monthly SUMMARY data download

--- NYC_Taxi_Analytics_Pipeline/streamlit_dashboard.py
+++ NYC_Taxi_Analytics_Pipeline/streamlit_dashboard.py
@@ -33,20 +33,20 @@ st.sidebar.header("Filters")
 year_selected = st.sidebar.selectbox("Select Year", [2025, 2026])
 
 # Create dataframes through Athena queries
-monthly_df = run_query(queries.KPI_MONTHLY_SUMMARY_QUERY)
-payment_df = run_query(queries.PAYMENT_METHOD_STATS_QUERY)
-payment_df = payment_df.set_index("payment_method")
+monthly_summary_df = run_query(queries.KPI_MONTHLY_SUMMARY_QUERY)
+payment_summary_df = run_query(queries.PAYMENT_METHOD_STATS_QUERY)
+payment_summary_df = payment_summary_df.set_index("payment_method")
 hourly_df = run_query(queries.HOURLY_TRIPS_QUERY)
 # ===================
 # Derived Metrics
 # ===================
 
-monthly_df["month_date"] = pd.to_datetime(monthly_df["month_date"])
-monthly_df["month_num"] = monthly_df["month_date"].dt.month
+monthly_summary_df["month_date"] = pd.to_datetime(monthly_summary_df["month_date"])
+monthly_summary_df["month_num"] = monthly_summary_df["month_date"].dt.month
 # this may have a compiler warning of unexpected type (str, property), but this is because type checkers don't know the exact typing of the data
 # the month value (a categorical string key in the Athena dataset) as an int
 
-monthly_df = monthly_df.sort_values("month_date")
+monthly_summary_df = monthly_summary_df.sort_values("month_date")
 
 # ==========
 # Indexed DFs for reusability
@@ -67,7 +67,7 @@ st.subheader("Overview")
 col1, col2, col3 = st.columns(3)
 
 # Make the revenue fit on the screen
-total_revenue = monthly_df["total_revenue"].sum()
+total_revenue = monthly_summary_df["total_revenue"].sum()
 if total_revenue >= 1_000_000_000:
     revenue_display = f"${total_revenue/1_000_000_000:.3f}B"
 elif total_revenue >= 1_000_000:
@@ -78,8 +78,8 @@ else:
     revenue_display = f"${total_revenue:.3f}"
 
 col1.metric("Total Revenue (USD)", revenue_display)
-col2.metric("Total Trips", f"{monthly_df['total_trips'].sum():,}")
-col3.metric("Avg Revenue (USD) / Trip", f"${monthly_df['avg_fare'].mean():.2f}")
+col2.metric("Total Trips", f"{monthly_summary_df['total_trips'].sum():,}")
+col3.metric("Avg Revenue (USD) / Trip", f"${monthly_summary_df['avg_fare'].mean():.2f}")
 
 # ========================
 # REVENUE TRENDS
@@ -91,22 +91,22 @@ col3.metric("Avg Revenue (USD) / Trip", f"${monthly_df['avg_fare'].mean():.2f}")
 # Monthly Total Revenue
 st.subheader("Monthly Total Revenue (USD)")
 st.caption("X-axis: Month | Y-Axis: Total Monthly Revenue (USD)")
-st.bar_chart(monthly_df["total_revenue"])
+st.bar_chart(monthly_summary_df["total_revenue"])
 
 # Monthly Cumulative Revenue
 st.subheader("Monthly Cumulative Revenue (USD)")
 st.caption("X-axis: Month | Y-Axis: Monthly Cumulative Revenue (USD)")
-st.bar_chart(monthly_df["cumulative_revenue"])
+st.bar_chart(monthly_summary_df["cumulative_revenue"])
 
 # Monthly Trips
 st.subheader("Monthly Trips")
 st.caption("X-axis: Month | Y-axis: Monthly Trips")
-st.bar_chart(monthly_df["total_trips"])
+st.bar_chart(monthly_summary_df["total_trips"])
 
 # Monthly Avg Fare
 st.subheader("Monthly Avg Revenue (USD) per Trip")
 st.caption("X-axis: Month | Y-axis: Monthly Avg Revenue (USD) per Trip")
-st.bar_chart(monthly_df["avg_fare"])
+st.bar_chart(monthly_summary_df["avg_fare"])
 
 # Avg Speed by Hour
 st.subheader("Hourly Avg Speed (MPH)")
@@ -119,31 +119,30 @@ st.line_chart(hourly_df.set_index("hour_of_day")["avg_speed_mph"])
 
 # Count of Trips by Payment Type
 st.subheader("Number of trips by Payment Method")
-st.table(payment_df[["total_trips"]])
+st.table(payment_summary_df[["total_trips"]])
 
 # Avg Distance by Payment Type
 st.subheader("Avg Distance (Miles) by Payment Method")
 st.caption("X-axis: Payment Method | Y-axis: Avg Distance (Miles) by Payment Method")
-st.bar_chart(payment_df[["avg_distance"]])
+st.bar_chart(payment_summary_df[["avg_distance"]])
 
 # Avg Fare by Payment Type
 st.subheader("Avg Fare (USD) by Payment Method")
 st.caption("X-axis: Payment Method | Y-axis: Avg Fare (USD) by Payment Method")
-st.bar_chart(payment_df[["avg_fare"]])
+st.bar_chart(payment_summary_df[["avg_fare"]])
 
 # Standard Deviation of Fare by Payment Type
 st.subheader("Standard Deviation of Fare (USD) by Payment Method")
 st.caption("X-axis: Payment Method | Y-axis: Fare Standard Deviation (USD) by Payment Method")
-st.bar_chart(payment_df[["stdev_fare"]])
+st.bar_chart(payment_summary_df[["stdev_fare"]])
 
 # =============
 # Data Preview
 # =============
-st.subheader("🔍 Data Preview")
+st.subheader("Data Preview (50 rows)")
 
-df_preview = monthly_df.head(100)
+df_preview = pd.read_csv("preprocessed_preview_data/nyc_taxi_preview_50.csv")
 st.dataframe(df_preview)
-
 
 preview_csv = df_preview.to_csv(index=False).encode("utf-8")
 st.caption("Download the Preview as a CSV")
@@ -155,29 +154,26 @@ st.download_button(
 )
 
 # Yearly download
-st.subheader("Yearly Data Download")
-st.caption("WARNING: *VERY* Big File. Download the Full Year as a CSV.")
+st.subheader("Yearly Data Summary Download")
 
-if st.button("Download Yearly Data"):
-    yearly_data = monthly_df
+if st.button("Download Yearly Summary Data"):
+    yearly_data_df = monthly_summary_df[monthly_summary_df["year"] == year_selected]
     st.download_button(
-        label=f"Download {year_selected} Data as CSV",
-        data=yearly_data.to_csv(index=False).encode("utf-8"),
+        label=f"Download {year_selected} Summary Data as CSV",
+        data=yearly_data_df.to_csv(index=False).encode("utf-8"),
         file_name=f"nyc_taxi_year_{year_selected}.csv",
         mime="text/csv"
     )
 
 # Monthly data download
-st.subheader("📥 Download Data by Month")
-
+st.subheader("📥 Download Monthly Summary Data")
+st.caption("If you want the raw monthly data, get it from the NYC Taxi Dataset website / Yellow Taxi Dataset")
 month_selected = st.number_input("Month", min_value=1, max_value=12, step=1)
 
-if st.button("Download Monthly Data"):
-
-    selected_month_df = monthly_df[monthly_df["month_num"] == month_selected]
-    st.caption(f"WARNING: Big File. Download Data for Selected Month and Year")
+if st.button("Download Monthly Summary Data"):
+    selected_month_df = monthly_summary_df[monthly_summary_df["month_num"] == month_selected and monthly_summary_df["year"] == year_selected]
     st.download_button(
-        label=f"Download {year_selected}_{month_selected} Data as CSV",
+        label=f"Download {year_selected}_{month_selected} Summary Data as CSV",
         data=selected_month_df.to_csv(index=False).encode("utf-8"),
         file_name=f"nyc_taxi_{year_selected}/{month_selected:02d}.csv",
         mime="text/csv"
