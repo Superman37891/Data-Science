7dd81ffcbcad504fdcf1bd89f7b568d4db8e0bd2
Superman37891 <sarthakbansal000@gmail.com>
2026-05-29 19:11:23 -0500

    Made it clearer in streamlit title overview is only for year selected

--- NYC_Taxi_Analytics_Pipeline/streamlit_dashboard.py
+++ NYC_Taxi_Analytics_Pipeline/streamlit_dashboard.py
@@ -63,7 +63,7 @@ st.caption("Interactive dashboard analyzing NYC taxi revenue, trips, fares, and 
 # ========================
 # KPI SUMMARY
 # ========================
-st.subheader("Overview")
+st.subheader(f"Overview for year {year_selected}")
 
 col1, col2, col3 = st.columns(3)
 
