fbe5a99cca5d19a32f045d33ffc748b7b72b47a7
Superman37891 <sarthakbansal000@gmail.com>
2026-07-02 17:51:25 -0500

    Attempted to fix GitHub logging again

--- NYC_Taxi_Analytics_Pipeline/src/utils/logger.py
+++ NYC_Taxi_Analytics_Pipeline/src/utils/logger.py
@@ -2,14 +2,17 @@ import logging
 from pathlib import Path
 
 def setup_logger():
-    LOG_DIR = Path("logs")
+    LOG_DIR = Path.cwd() / "logs"
     LOG_DIR.mkdir(exist_ok=True)
+
+    log_file = LOG_DIR / "pipeline.log"
 
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
         handlers=[
-            logging.FileHandler(LOG_DIR / "pipeline.log"),
+            logging.FileHandler(log_file, mode="a"),
             logging.StreamHandler()
-        ]
+        ],
+        force=True  # 🔥 THIS IS CRITICAL (Python 3.8+)
     )
