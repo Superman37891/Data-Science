1cd32d0b72b9ae1e566dffa377629c858df7367a
Superman37891 <sarthakbansal000@gmail.com>
2026-07-02 14:49:21 -0500

    Added more logging to explain failure in action

--- NYC Taxi Analytics Pipeline/pipeline.py
+++ NYC Taxi Analytics Pipeline/pipeline.py
@@ -1,11 +1,16 @@
 from src.data_ingestion.download_tlc import get_latest_available, stream_download_to_s3
 from src.etl.process_taxi_data import process_file_from_s3_with_retry
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 def main():
     year, month = get_latest_available()
     # skip already processed data
 
     if year == -1:
+        logger.warning("No data available for this year and month")
         raise RuntimeError("No available TLC dataset found\n")
 
     raw_key = stream_download_to_s3(year, month)
