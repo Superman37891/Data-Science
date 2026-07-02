c02dabede2c527aad5b3171c00d0fc861c7d7cfd
Superman37891 <sarthakbansal000@gmail.com>
2026-07-02 16:19:54 -0500

    Fixed env variables and updated files accordingly

--- NYC_Taxi_Analytics_Pipeline/pipeline.py
+++ NYC_Taxi_Analytics_Pipeline/pipeline.py
@@ -13,9 +13,6 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-import os
-print("TLC_BASE_URL =", os.getenv("TLC_BASE_URL"))
-
 def main():
     logger.info("Pipeline started")
     year, month = get_latest_available()
